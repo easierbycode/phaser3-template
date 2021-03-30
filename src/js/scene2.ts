d7677a54f2134a16aab7f28d2c56b2d412eb58a4
easierbycode <daniel@easierbycode.com>
2021-03-30 14:44:47 -0700

    clean up

--- src/js/scene2.ts
+++ src/js/scene2.ts
@@ -6,18 +6,11 @@ export default class Scene2 extends Phaser.Scene {
     }
 
     create() {
-        this.events.on('enterfullscreen', () => {
-            screen.orientation.lock('landscape')
-            .catch((err) => {alert(err)})
-            .then(() => {
-                var platforms = this.physics.add.staticGroup();
-
-                platforms.create(this.scale.width / 2, this.scale.height / 2, 'ground').refreshBody();
-            });
-        });
-        
-        // this.scale.lockOrientation('landscape-primary');
         this.scale.startFullscreen();
+
+        var platforms = this.physics.add.staticGroup();
+
+        platforms.create(this.scale.width / 2, this.scale.height / 2, 'ground').refreshBody();
     }
 
 }
